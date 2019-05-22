bc4a2e3b94add46e681fbed0a99a71170c8e0223
Karthik Chowdary Paladugu <karthik99th.tnk@gmail.com>
2019-05-22 15:08:47 +0530

    made campaign dynamic

--- web/src/Components/Campaign/Campaign.js
+++ web/src/Components/Campaign/Campaign.js
@@ -5,6 +5,15 @@ import { Card, Badge, Button, Nav } from "react-bootstrap";
 import "./Campaign.css";
 
 class Campaign extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeCampaigns: [],
+      archiveCampaigns: [],
+      completeCampaigns: [],
+    };
+  }
+
   componentWillMount() {
     fetch("http://10.2.135.75:5000/displayCampaign", {
       method: "GET",
@@ -13,11 +22,144 @@ class Campaign extends Component {
         return res.json();
       })
       .then(data => {
-        console.log(data);
+        let active = [],
+          complete = [],
+          archive = [];
+        for (let i = 0; i < data.length; i++) {
+          if (data[i][2] === "active") {
+            active.push(data[i]);
+          } else if (data[i][2] === "archive") {
+            archive.push(data[i]);
+          } else {
+            complete.push(data[i]);
+          }
+        }
+        this.setState({
+          activeCampaigns: active,
+          archiveCampaigns: archive,
+          completeCampaigns: complete,
+        });
+        console.log(this.state.activeCampaigns);
+        console.log(this.state.archiveCampaigns);
+        console.log(this.state.completeCampaigns);
       })
       .catch(er => {
         console.log(er);
       });
+  }
+
+  createActive = () => {
+    let row = [];
+    let length = this.state.activeCampaigns.length;
+    for (let i = 0; i < this.state.activeCampaigns.length; i++) {
+      let col = [];
+      for (let j = 0; j < (length>3)?3:length; j++) {
+        col.push(
+          <Col sm={6} lg={4}>
+            <Card className="campaigns" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={require("../../img/campaign.png")} />
+              <Card.Body>
+                <Card.Title>
+                  {this.state.activeCampaigns[i][0]}
+                  { this.state.activeCampaigns[i][5] === "yes" && <Badge pill variant="danger">
+                    Paid
+                  </Badge>}
+                </Card.Title>
+                <Card.Subtitle>
+                {this.state.activeCampaigns[i][3]===1?("English"):(this.state.activeCampaigns[i][3]===2?("Hindi"):("Telugu"))}
+                </Card.Subtitle>
+                <Card.Text>
+                  {this.state.activeCampaigns[i][7]}
+                </Card.Text>
+                <Card.Text>Duration : {this.state.activeCampaigns[i][8]}hrs</Card.Text>
+                <Button variant="primary">Start Contest</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      }
+      length -= 3;
+      row.push(
+        <Row>{col}</Row>
+      )
+    }
+    return row;
+  };
+
+  createArchive(){
+    let row = [];
+    let length = this.state.archiveCampaigns.length;
+    for (let i = 0; i < this.state.archiveCampaigns.length; i++) {
+      let col = [];
+      for (let j = 0; j < (length>3)?3:length; j++) {
+        col.push(
+          <Col sm={6} lg={4}>
+            <Card className="campaigns" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={require("../../img/campaign.png")} />
+              <Card.Body>
+                <Card.Title>
+                  {this.state.archiveCampaigns[i][0]}
+                  { this.state.archiveCampaigns[i][5] === "yes" && <Badge pill variant="danger">
+                    Paid
+                  </Badge>}
+                </Card.Title>
+                <Card.Subtitle>
+                {this.state.archiveCampaigns[i][3]===1?("English"):(this.state.archiveCampaigns[i][3]===2?("Hindi"):("Telugu"))}
+                </Card.Subtitle>
+                <Card.Text>
+                  {this.state.archiveCampaigns[i][7]}
+                </Card.Text>
+                <Card.Text>Duration : {this.state.archiveCampaigns[i][8]}hrs</Card.Text>
+                <Button variant="primary">Start Contest</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      }
+      length -= 3;
+      row.push(
+        <Row>{col}</Row>
+      )
+    }
+    return row;
+  }
+
+  createComplete(){
+    let row = [];
+    let length = this.state.completeCampaigns.length;
+    for (let i = 0; i < this.state.completeCampaigns.length; i++) {
+      let col = [];
+      for (let j = 0; j < (length>3)?3:length; j++) {
+        col.push(
+          <Col sm={6} lg={4}>
+            <Card className="campaigns" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={require("../../img/campaign.png")} />
+              <Card.Body>
+                <Card.Title>
+                  {this.state.completeCampaigns[i][0]}
+                  { this.state.completeCampaigns[i][5] === "yes" && <Badge pill variant="danger">
+                    Paid
+                  </Badge>}
+                </Card.Title>
+                <Card.Subtitle>
+                {this.state.completeCampaigns[i][3]===1?("English"):(this.state.completeCampaigns[i][3]===2?("Hindi"):("Telugu"))}
+                </Card.Subtitle>
+                <Card.Text>
+                  {this.state.completeCampaigns[i][7]}
+                </Card.Text>
+                <Card.Text>Duration : {this.state.completeCampaigns[i][8]}hrs</Card.Text>
+                <Button variant="primary">Start Contest</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      }
+      length -= 3;
+      row.push(
+        <Row>{col}</Row>
+      )
+    }
+    return row;
   }
 
   render() {
@@ -29,10 +171,10 @@ class Campaign extends Component {
               <Nav.Link href="#active">Active</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="#upcoming">Upcoming</Nav.Link>
+              <Nav.Link href="#upcoming">Archive</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="#past">Past</Nav.Link>
+              <Nav.Link href="#past">Complete</Nav.Link>
             </Nav.Item>
           </Nav>
         </Card.Header>
@@ -40,195 +182,15 @@ class Campaign extends Component {
           <h1 id="active" className="event-head">
             Active Campaigns
           </h1>
-          <Row>
-            <Col sm={6} lg={4}>
-              <Card className="campaigns" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={require("../../img/campaign.png")}
-                />
-                <Card.Body>
-                  <Card.Title>
-                    Campaign 1{" "}
-                    <Badge pill variant="danger">
-                      Paid
-                    </Badge>
-                  </Card.Title>
-                  <Card.Subtitle>telugu</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Text>Duration : 12hrs</Card.Text>
-                  <Button variant="primary">Start Contest</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col sm={6} lg={4}>
-              <Card className="campaigns" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={require("../../img/campaign.png")}
-                />
-                <Card.Body>
-                  <Card.Title>Campaign 1</Card.Title>
-                  <Card.Subtitle>telugu</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Text>Duration : 12hrs</Card.Text>
-                  <Button variant="primary">Start Contest</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col sm={6} lg={4}>
-              <Card className="campaigns" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={require("../../img/campaign.png")}
-                />
-                <Card.Body>
-                  <Card.Title>Campaign 1</Card.Title>
-                  <Card.Subtitle>telugu</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Text>Duration : 12hrs</Card.Text>
-                  <Button variant="primary">Start Contest</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+          {this.createActive()}
           <h1 id="upcoming" className="event-head">
-            Upcoming Campaigns
+            Archive Campaigns
           </h1>
-          <Row>
-            <Col sm={6} lg={4}>
-              <Card className="campaigns" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={require("../../img/campaign.png")}
-                />
-                <Card.Body>
-                  <Card.Title>
-                    Campaign 1{" "}
-                    <Badge pill variant="danger">
-                      Paid
-                    </Badge>
-                  </Card.Title>
-                  <Card.Subtitle>telugu</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Text>Duration : 12hrs</Card.Text>
-                  <Button variant="primary">Start Contest</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col sm={6} lg={4}>
-              <Card className="campaigns" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={require("../../img/campaign.png")}
-                />
-                <Card.Body>
-                  <Card.Title>Campaign 1</Card.Title>
-                  <Card.Subtitle>telugu</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Text>Duration : 12hrs</Card.Text>
-                  <Button variant="primary">Start Contest</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col sm={6} lg={4}>
-              <Card className="campaigns" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={require("../../img/campaign.png")}
-                />
-                <Card.Body>
-                  <Card.Title>Campaign 1</Card.Title>
-                  <Card.Subtitle>telugu</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Text>Duration : 12hrs</Card.Text>
-                  <Button variant="primary">Start Contest</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+          {this.createArchive()}
           <h1 id="past" className="event-head">
-            Past Campaigns
+            Complete Campaigns
           </h1>
-          <Row>
-            <Col sm={6} lg={4}>
-              <Card className="campaigns" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={require("../../img/campaign.png")}
-                />
-                <Card.Body>
-                  <Card.Title>
-                    Campaign 1{" "}
-                    <Badge pill variant="danger">
-                      Paid
-                    </Badge>
-                  </Card.Title>
-                  <Card.Subtitle>telugu</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Text>Duration : 12hrs</Card.Text>
-                  <Button variant="primary">Start Contest</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col sm={6} lg={4}>
-              <Card className="campaigns" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={require("../../img/campaign.png")}
-                />
-                <Card.Body>
-                  <Card.Title>Campaign 1</Card.Title>
-                  <Card.Subtitle>telugu</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Text>Duration : 12hrs</Card.Text>
-                  <Button variant="primary">Start Contest</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col sm={6} lg={4}>
-              <Card className="campaigns" style={{ width: "18rem" }}>
-                <Card.Img
-                  variant="top"
-                  src={require("../../img/campaign.png")}
-                />
-                <Card.Body>
-                  <Card.Title>Campaign 1</Card.Title>
-                  <Card.Subtitle>telugu</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Text>Duration : 12hrs</Card.Text>
-                  <Button variant="primary">Start Contest</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+          {this.createComplete()}
         </Container>
       </Card>
     );
